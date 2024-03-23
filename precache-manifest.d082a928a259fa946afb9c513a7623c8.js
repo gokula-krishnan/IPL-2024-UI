d7e8bc7e924ac57e7a2f548f427a88561b608cf3
GK <gokula_krishnan@trimble.com>
2024-03-23 22:52:57 +0530

    Updates

---
+++ precache-manifest.d082a928a259fa946afb9c513a7623c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73910e150cbff157ddcd",
+    "url": "/IPL-2024-UI/static/css/main.d1caa4f2.chunk.css"
+  },
+  {
+    "revision": "73910e150cbff157ddcd",
+    "url": "/IPL-2024-UI/static/js/main.73910e15.chunk.js"
+  },
+  {
+    "revision": "48b7aa7a38af26bea41b",
+    "url": "/IPL-2024-UI/static/js/runtime~main.48b7aa7a.js"
+  },
+  {
+    "revision": "dfd2890e9d31265d1dcb",
+    "url": "/IPL-2024-UI/static/css/2.b93a0912.chunk.css"
+  },
+  {
+    "revision": "dfd2890e9d31265d1dcb",
+    "url": "/IPL-2024-UI/static/js/2.dfd2890e.chunk.js"
+  },
+  {
+    "revision": "7757a72d561fe2e7dcca23f9460b0677",
+    "url": "/IPL-2024-UI/index.html"
+  }
+];
